73b2f84860ed8222b0287d989b6f55165ea88251
Wallinson Deives Batista Lima <wdeives@rams.colostate.edu>
2016-11-10 10:52:51 -0300

    Projeto Criado

---
+++ app/src/main/java/saacs/ufc/com/br/saacs/MainActivity.java
@@ -0,0 +1,13 @@
+package saacs.ufc.com.br.saacs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
