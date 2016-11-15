75efc94e8246358c4f6572267ed9472ce9818f87
dnmendes76 <dnmendes76@gmail.com>
2016-11-15 15:35:53 -0300

    model3

---
+++ app/src/main/java/model/GrupoFamiliar.java
@@ -0,0 +1,25 @@
+package model;
+
+/**
+ * Created by pc on 15/11/16.
+ */
+
+public class GrupoFamiliar {
+
+
+
+}
+
+
+//    Cadastro grupo Familiar:
+//
+//        Lista de pessoas
+//        Telefones para contato
+//        Endereço - Rua, Número e Bairro, Cidade, Estado
+//        Localização - Urbana| Rural
+//        Condições de Moradia - Própria|Financiado|Alugado|Cedido|Ocupação|Outra
+//        Tipo de Domicílio - Casa|Apartamento|Cômodo|Outro
+//        Energia Elétrica?
+//        Saneamento Básico?
+//        Destino do Lixo?
+//        Animais no Domicílio - Sim|Não -- Se sim Qual|Quais?
