d6b9ec35c392ab56b7d7a9b274573063cd69f117
dnmendes76 <dnmendes76@gmail.com>
2016-12-03 15:30:04 -0300

    Adicionando Atributos 2.0

--- app/src/main/java/saacs/ufc/com/br/saacs/dao/PessoaDAO.java
+++ app/src/main/java/saacs/ufc/com/br/saacs/dao/PessoaDAO.java
@@ -72,13 +72,17 @@ public class PessoaDAO {
                 p.setNumSUS(cursor.getLong(cursor.getColumnIndex("numSUS")));
                 p.setNome(cursor.getString(cursor.getColumnIndex("nome")));
                 p.setDataNascimento(cursor.getString(cursor.getColumnIndex("dataNascimento")));
-                //p.setNumSUSResponsavel(cursor.getLong(cursor.getColumnIndex("numSUSResponsavel")));
+                p.setNumeroNis(cursor.getLong(cursor.getColumnIndex("numeroNis")));
                 p.setSexo(cursor.getString(cursor.getColumnIndex("sexo")));
+                p.setSexo(cursor.getString(cursor.getColumnIndex("etnia")));
                 p.setNacionalidade(cursor.getString(cursor.getColumnIndex("nacionalidade")));
+                p.setSexo(cursor.getString(cursor.getColumnIndex("paisDeOrigem")));
                 p.setCidadeNatal(cursor.getString(cursor.getColumnIndex("cidadeNatal")));
+                p.setCidadeNatal(cursor.getString(cursor.getColumnIndex("estado")));
                 p.setTelefone(cursor.getString(cursor.getColumnIndex("telefone")));
                 p.setEmail(cursor.getString(cursor.getColumnIndex("email")));
 
+                p.setEmail(cursor.getString(cursor.getColumnIndex("nomeDaMae")));
                 p.setResponsavelFamiliar(cursor.getInt(cursor.getColumnIndex("responsavelFamiliar")) == 1);
                 p.setRelacaoParentRF(cursor.getString(cursor.getColumnIndex("relacaoParentRF")));
                 p.setProfissao(cursor.getString(cursor.getColumnIndex("profissao")));
@@ -86,16 +90,11 @@ public class PessoaDAO {
                 p.setSituacaoMercado(cursor.getString(cursor.getColumnIndex("situacaoMercado")));
 
                 SituacaoSaude saude = saudeDAO.buscar(cursor.getLong(cursor.getColumnIndex("id_saude")));
-
                 p.setSaude(saude);
-
                 pessoas.add(p);
             } while(cursor.moveToNext());
         }
-
         db.close();
-
-        // fazer uma função para gerar a lista? return toList(cursor)
         return pessoas;
 
     }
@@ -127,12 +126,16 @@ public class PessoaDAO {
         values.put("numSUS", pessoa.getNumSUS());
         values.put("nome", pessoa.getNome());
         values.put("dataNascimento", pessoa.getDataNascimento());
-        //values.put("numSUSResponsavel", pessoa.getNumSUSResponsavel());
+        values.put("numeroNis", pessoa.getNumeroNis());
         values.put("sexo", pessoa.getSexo());
+        values.put("etnia", pessoa.getEtnia());
         values.put("nacionalidade", pessoa.getNacionalidade());
+        values.put("paisDeOrigem", pessoa.getPaisDeOrigem());
         values.put("cidadeNatal", pessoa.getCidadeNatal());
+        values.put("estado", pessoa.getEstado());
         values.put("telefone", pessoa.getTelefone());
         values.put("email", pessoa.getEmail());
+        values.put("nomeDaMae", pessoa.getNomedaMae());
         values.put("responsavelFamiliar", pessoa.isResponsavelFamiliar());
         values.put("relacaoParentRF", pessoa.getRelacaoParentRF());
         values.put("profissao", pessoa.getProfissao());
