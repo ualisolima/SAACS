cceed12dade1161ac78dcab32638f45965795673
dnmendes76 <dnmendes76@gmail.com>
2016-12-11 16:51:25 -0300

    Consultas 1.0

--- app/src/main/java/saacs/ufc/com/br/saacs/dao/ConsultasRelatorio.java
+++ app/src/main/java/saacs/ufc/com/br/saacs/dao/ConsultasRelatorio.java
@@ -19,8 +19,18 @@ public class ConsultasRelatorio {
 
     public int qtdPessoaPorSexo(String sexo){
         int qtd = 0;
-
         String query = "SELECT COUNT(*) FROM pessoa where sexo = "+sexo;
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        if (cursor.moveToFirst())
+            qtd = cursor.getInt(0);
+        db.close();
+        return qtd;
+    }
+
+    public int qtdPessoaSituacao(String coluna){
+        int qtd = 0;
+        String query = "SELECT COUNT(*) FROM pessoa p, situacao_saude s where p.id_saude = s.id_situacao and s."+coluna+" = 1";
         db = dbHelper.getReadableDatabase();
         Cursor cursor = db.rawQuery(query, null);
         if (cursor.moveToFirst())
